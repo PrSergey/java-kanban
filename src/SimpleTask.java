3aad769f4484fc46b0e03538d73e78732e619688
Sergey <Serik1995@yandex.ru>
2022-10-10 19:43:42 +0300

    fix: внесены исправления после первого ревью

--- src/SimpleTask.java
+++
@@ -1,8 +0,0 @@
-public class SimpleTask extends Task {
-    protected String status;
-
-    public SimpleTask(String title, String description, String status) {
-        super(title, description);
-        this.status = status;
-    }
-}
